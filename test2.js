6ab4470dc1930e75827698076f679b324e4fb892
hodaka250 <95903411+hodaka250@users.noreply.github.com>
2023-09-19 19:01:34 +0900

    kadai
    js

--- test2.js
+++ test2.js
@@ -1,19 +1,18 @@
-function changelang() {
-  if(document.getElementById("change"));{
-    id = document.getElementById('change').value;
-    if (id == "selected_jpn") {
-      //文字1を表示
-      document.getElementById("t1").style.display = "";
-      document.getElementById("t2").style.display = "none";
-
-    } else if (id == "selected_en") {
-      document.getElementById("t1").style.display = "none";
-      document.getElementById("t2").style.display = "";
-
-    } else if (id == "selected_all") {
-      document.getElementById("t1").style.display = "";
-      document.getElementById("t2").style.display = "";
-     }
+function viewChange(){
+  if(document.getElementById('sample')){
+      id = document.getElementById('sample').value;
+      if(id == 'selected_jpn'){
+          document.getElementById('jp').style.display = "";
+          document.getElementById('en').style.display = "none";
+      }else if(id == 'selected_en'){
+          document.getElementById('jp').style.display = "none";
+          document.getElementById('en').style.display = "";
+      }
+      else if(id == 'selected_all'){
+          document.getElementById('jp').style.display = "";
+          document.getElementById('en').style.display = "";
+      }
   }
-window.onload = changelang;
+
+window.onload = viewChange;
 }
